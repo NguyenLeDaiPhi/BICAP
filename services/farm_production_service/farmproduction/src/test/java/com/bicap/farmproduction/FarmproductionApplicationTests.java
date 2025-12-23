8dfdba012c9eba1ddce7b71b991ceddea6427ed1
TuanNguyen-dev28 <nat281205@gmail.com>
2025-12-23 22:35:00 +0700

    commit to merge

---
+++ services/farm_production_service/farmproduction/src/test/java/com/bicap/farmproduction/FarmproductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bicap.farmproduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmproductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
