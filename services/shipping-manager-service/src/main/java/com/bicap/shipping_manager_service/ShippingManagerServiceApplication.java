8efec1c9c05bc6d4b804b1c6fd17106187e79f94
TuanNguyen-dev28 <nat281205@gmail.com>
2026-01-15 00:46:40 +0700

    push shipping-manager-service file

---
+++ services/shipping-manager-service/src/main/java/com/bicap/shipping_manager_service/ShippingManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bicap.shipping_manager_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippingManagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShippingManagerServiceApplication.class, args);
+	}
+
+}
