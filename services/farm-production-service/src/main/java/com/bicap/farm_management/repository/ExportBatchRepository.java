c353087bdcd8f3e971bf537772f9b964a80b0ab1
Baobllack033@gmail.com <baoblack033@gmail.com>
2026-01-22 21:54:38 +0700

    commit file

--- services/farm-production-service/src/main/java/com/bicap/farm_management/repository/ExportBatchRepository.java
+++ services/farm-production-service/src/main/java/com/bicap/farm_management/repository/ExportBatchRepository.java
@@ -1,9 +1,14 @@
 package com.bicap.farm_management.repository;
 
-import com.bicap.farm_management.entity.ExportBatch;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.bicap.farm_management.entity.ExportBatch; // Nhớ import List
+
 @Repository
 public interface ExportBatchRepository extends JpaRepository<ExportBatch, Long> {
+    // THÊM DÒNG NÀY: Để tìm các đợt xuất hàng thuộc về một lô sản xuất cụ thể
+    List<ExportBatch> findByProductionBatchId(Long productionBatchId);
 }
